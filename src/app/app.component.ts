7474e6314010c909941384d79d15397496ebf788
eprelofcdesktop <eprelofcdesktop@am.com>
2018-09-03 13:45:00 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { tap, skipWhile } from 'rxjs/operators';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  throttle = 300;
+  scrollDistance = 1;
+
+  public news: Array<any> = [];
+
+  public httpReqestInProgress: boolean = false;
+
+  // Source data
+  private _done = new BehaviorSubject(false);
+  private _loading = new BehaviorSubject(false);
+  private _data = new BehaviorSubject([]);
+
+  // Observable data
+  data: Observable<any>;
+  done: Observable<boolean> = this._done.asObservable();
+  loading: Observable<boolean> = this._loading.asObservable();
+
+  private currentPage = 1;
+  private _lastPage = 1;
+
+  constructor(private http: HttpClient) { }
+
+  public ngOnInit() {
+    this.getNews(
+      this.currentPage,
+      (news) => {
+        this.news = this.news.concat(news);
+      });
+  }
+
+  onScrollDown (ev): void {
+    console.log('scrolled down!!', ev);
+    this._loading.next(true);
+    this.getNews(
+      this.currentPage,
+      (news) => {
+        this.news = this.news.concat(news);
+        this._loading.next(false);
+        this._lastPage = this.currentPage;
+        this.currentPage = this._lastPage + 1;
+        console.log('page ' + this.currentPage);
+      });
+  }
+
+  private getNews(page: number = 1, saveResultsCallback: (news) => void) {
+    return this.http.get(`https://node-hnapi.herokuapp.com/news?page=${page}`).pipe(
+      skipWhile(() => this.httpReqestInProgress),
+      tap(() => { this.httpReqestInProgress = true; })
+    ).subscribe((news: any[]) => {
+        // this.currentPage++;
+        saveResultsCallback(news);
+        this.httpReqestInProgress = false;
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoadingSpinnerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InfiniteScrollModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// platformBrowserDynamic().bootstrapModule(AppModule);
